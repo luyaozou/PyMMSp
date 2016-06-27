14b7bad9860666e30d7f859742d1aa12e67a8ecd
luyaozou <zouluyao@hotmail.com>
2016-06-26 21:42:51 -0400

    Initial Commit

---
+++ PySpec.py
@@ -0,0 +1,21 @@
+"""
+  Integrated Python GUI for spectral analysis.
+  Designed functionalities:
+    > Plot spectra (x,y) file
+    > Fit spectral lines to common lineshape functions (Gaussian/Lorentzian/Voigt)
+    > Baseline removal
+    > Peak selection
+    > Catalog simulation (JPL/CDMS)
+    > 
+    
+  Package Requirments:
+    > numpy 1.8+
+    > scipy 0.16+
+    > PyQt5 
+    > PyQtGraph
+    
+  Written by Luyao Zou @ https://github.com/luyaozou
+"""
+
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
